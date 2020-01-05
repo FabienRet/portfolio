3050f56b96a5549e535e29626b9ff35ef34cfb89
fabienRet <fabienretailleau@hotmail.Fr>
2020-01-05 14:55:37 +0100

    start project

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    /**
+     * @Route("/", name="front")
+     */
+    public function index()
+    {
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    /**
+     * @Route("/cv", name="cv")
+     */
+    public function cv(){
+        return $this->render('front/cv.html.twig');
+    }
+}
